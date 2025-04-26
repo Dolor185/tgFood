d10b4b6c7351f63688c8866354939d70bfc57287
Dolor185 <kir.anishenko@gmail.com>
2025-04-25 18:38:59 -0600

    Refactor FoodHistory schema to use a Map for history and update reset logic to store daily nutrient entries

--- DB/FoodHistory.js
+++ DB/FoodHistory.js
@@ -1,26 +1,33 @@
 const mongoose = require("mongoose");
 
 const foodHistorySchema = new mongoose.Schema({
-  userId: { type: Number, required: true },
-  date: { type: Date, required: true },
-  products: [
-    {
-      name: String,
-      amount: Number,
-      metric_serving_unit: String,
-      calories: Number,
-      protein: Number,
-      fat: Number,
-      carbs: Number,
-    }
-  ],
-  total: {
-    calories: Number,
-    protein: Number,
-    fat: Number,
-    carbs: Number,
+  userId: { type: Number, required: true, unique: true },
+  history: {
+    type: Map, // Ключ: дата (строка), значение: объект total + продукты
+    of: new mongoose.Schema({
+      total: {
+        calories: Number,
+        protein: Number,
+        fat: Number,
+        carbs: Number,
+      },
+      products: [
+        {
+          name: String,
+          amount: Number,
+          metric_serving_unit: String,
+          calories: Number,
+          protein: Number,
+          fat: Number,
+          carbs: Number,
+        }
+      ]
+    }),
+    default: {},
   }
 });
+
+foodHistorySchema.index({userId:1})
 
 const FoodHistory = mongoose.model("FoodHistory", foodHistorySchema);
 module.exports = FoodHistory;
